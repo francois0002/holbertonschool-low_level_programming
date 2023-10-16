4ae0628b5a7f61f234bd286750ea06a81852398f
francois0002 <francois.gosselin.lille@gmail.com>
2023-10-16 12:23:07 +0200

    test variable unsigned

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -6,23 +6,24 @@
  * Description: description
  * Return:  0
  */
-int main(void)
+	 int main(void)
 {
 	int n;
+	unsigned last_digit = n%10;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n%10 > 5)
+	if(last_digit > 5)
 	{
-	printf("Last digit of %d is %d and is greater than 5\n",n, n%10);
+	printf("Last digit of %d is %d and is greater than 5\n",n, last_digit);
 	}
-	else if(n%10 == 0)
+	else if(last_digit == 0)
 	{
-	printf("Last digit of %d is %d and is 0\n",n, n%10);
+	printf("Last digit of %d is %d and is 0\n",n, last_digit);
 	}
-	else if (n%10 > 6 || n%10> 0)
+	else if (n%10 > 6 || last_digit> 0)
 	{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n",n, n%10);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n",n, last_digit);
 	}
 	return (0);
 }
