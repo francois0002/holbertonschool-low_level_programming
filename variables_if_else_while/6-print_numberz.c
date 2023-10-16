b53b8fe226fcb0f9cfcb7320271f7a06e3b77663
francois0002 <francois.gosselin.lille@gmail.com>
2023-10-16 17:01:09 +0200

    commit number

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -6,13 +6,13 @@
  * Description: afficher nombre
  * Return:  0
  */
-	 int main(void)
+int main(void)
 {
 int number;
 
 for (number = 0 ; number <= 9 ; number++)
 {
-putchar ('0'+number);
+putchar ('0' + number);
 }
 putchar ('\n');
 return (0);
