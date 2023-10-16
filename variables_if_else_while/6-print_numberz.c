9b3c671af2d1e9995b94b3c97e66727d9eb02c46
francois0002 <francois.gosselin.lille@gmail.com>
2023-10-16 15:41:51 +0200

    putchar nombre

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: afficher nombre
+ * Return:  0
+ */
+	 int main(void)
+{
+char number;
+
+for (number = 0 ; number <= 10 ; number++)
+{
+putchar (number);
+}
+putchar ('\n');
+return (0);
+}
