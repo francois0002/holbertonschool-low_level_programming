4223327f710d8e2acd399b4c68514b7839024481
francois0002 <francois.gosselin.lille@gmail.com>
2023-10-17 14:28:01 +0200

    first commit

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * _print_sign - Entry point
+ * Description: affiche le signe d'un nombre
+ * @n: variable d'un nombre entier positif ou négatif
+ * Return:  0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+	else if (n == 0)
+	{
+	_putchar('0');
+	return (0);
+	}
+	else
+	{
+	_putchar('-');
+	return (-1);
+	}
+}
