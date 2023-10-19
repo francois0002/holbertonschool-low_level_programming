7126d5ddb4545a4b8088e23f6de420e14d528b18
francois0002 <francois.gosselin.lille@gmail.com>
2023-10-19 12:19:00 +0200

    betty + compteur baissé de 1

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,41 +7,37 @@
  * Description: afficher chaque minute de la journée de minuit à 23h59
  * Return:  nothing
  */
- 
 	void jack_bauer(void)
 {
 	int compteur;
-	int heures_1=0;
+	int heures_1 = 0;
 	int heures_2 = 0;
 	int minutes_2 = 0;
 	int minutes_1 = 0;
-	
-	
+
 	putchar('0');
 	putchar('0');
 	putchar(':');
 	putchar('0');
 	putchar('0');
 	putchar('\n');
-	
-	for (compteur = 0 ; compteur < 1440 ; compteur++)
+	for (compteur = 0 ; compteur < 1439 ; compteur++)
 	{
-		minutes_2++ ;
-		if(minutes_2 > 9)
+		minutes_2++;
+		if (minutes_2 > 9)
 		{
 		minutes_2 = 0;
 		minutes_1++;
 		}
 		putchar(heures_1 + '0');
-		
-		if(minutes_1 > 5)
+		if (minutes_1 > 5)
 		{
 		minutes_1 = 0;
 		heures_2++;
 		}
 		putchar(heures_2 + '0');
 		putchar(':');
-		if(heures_2 > 9)
+		if (heures_2 > 9)
 		{
 		heures_2 = 0;
 		heures_1++;
@@ -49,6 +45,5 @@
 		putchar(minutes_1 + '0');
 		putchar(minutes_2 + '0');
 		putchar('\n');
-	}		
+	}
 }
-
