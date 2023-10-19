85ae63a091bfe439259f8b660446fcc01988a6ed
francois0002 <francois.gosselin.lille@gmail.com>
2023-10-19 11:38:20 +0200

    Commit for 24h

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,12 +7,39 @@
  * Description: afficher chaque minute de la journée de minuit à 23h59
  * Return:  m
  */
+ 
 void jack_bauer(void)
 {
-	for (seconde = 0 ; seconde < 9 ; seconde++)	
+	int compteur;
+	int heures_1=0;
+	int heures_2 = 0;
+	int minutes_2 = 0;
+	int minutes_1 = 0;
+	
+	for (compteur = 0 ; compteur < 1440 ; compteur++)
 	{
-		_putchar('0'+ seconde);
-		_putchar('\n');
-	}
-			
+		minutes_2++ ;
+		if(minutes_2 > 9)
+		{
+		minutes_2 = 0;
+		minutes_1++;
+		}
+		putchar(heures_1 + '0');
+		
+		if(minutes_1 > 5)
+		{
+		minutes_1 = 0;
+		heures_2++;
+		}
+		putchar(heures_2 + '0');
+		putchar(':');
+		if(heures_2 > 9)
+		{
+		heures_2 = 0;
+		heures_1++;
+		}
+		putchar(minutes_1 + '0');
+		putchar(minutes_2 + '0');
+		putchar('\n');
+	}		
 }
