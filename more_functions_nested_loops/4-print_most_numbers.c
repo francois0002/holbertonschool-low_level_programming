42972d515d34331cb3ff9fea52b950836fe25c77
francois0002 <francois.gosselin.lille@gmail.com>
2023-10-19 16:58:55 +0200

    first commit print most number

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_most_numbers- Entry point
+ * Description: affiche les nombres de 0 à 9 suivi d'une ligne mais ne pas afficher 2 et 4
+ * Return:  nothing
+ */
+void print_most_numbers(void)
+{
+	int compteur = 0;
+
+		while (compteur < 9)	
+		{
+		compteur++;
+		
+			if (compteur != 2 && compteur != 4)
+			{
+				_putchar(compteur + '0');
+			}
+	}
+	_putchar('\n');
+}
