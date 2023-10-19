a2b264a1e6d372a67f4d062fc4e1be24ee91b7a4
francois0002 <francois.gosselin.lille@gmail.com>
2023-10-19 14:38:52 +0200

    first commit

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * mul- Entry point
+ * Description: fonction qui multiplie 2 entier
+ * @a: entier a
+ * @b: entier b
+ * Return:  1 is c est un nombre sinon 0
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
