0c17858c23c6c03a95fe7be2bd73060f341bbd98
francois0002 <francois.gosselin.lille@gmail.com>
2023-10-23 18:37:15 +0200

    test avec longueur fonction

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,19 +6,22 @@
  * Description: afficher une chaine de caractère a l'envers
  * @s : chaine de caractère
  */
-
- 
 void print_rev(char *s)
 {
 int longueur = _strlen(s);
 int compteur;
-for (compteur = longueur ; compteur >= 0 ; compteur--)
+for (compteur = longueur - 1 ; compteur >= 0 ; compteur--)
 {
 _putchar(s[compteur]);
 }
 _putchar('\n');
 }
-
+/**
+ * _strlen - Entry point
+ * Description: renvoi la longeur d'une chaine de caractère
+ * @s : caractère de  la variable
+ * Return:  n
+ */
 int _strlen(char *s)
 {
 int longueur;
@@ -28,4 +31,20 @@ for (longueur = 0 ; *s != '\0' ; longueur++)
 }
 return (longueur);
 }
-
+/**
+* {
+*	int compteur = 0;
+*       int compteur2;
+*	for (compteur = 0 ; *s != '\0' ; compteur++)
+*	{
+*		s++;
+*	}
+*
+*	for (compteur2 = compteur - 1 ; compteur2 >= 0 ; compteur2--)
+*	{
+*		putchar(*s);
+*		s--;
+*	}
+* putchar('\n');
+* }
+*/
