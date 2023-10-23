80231e3515e597aa1381606c7d42922032b87562
francois0002 <francois.gosselin.lille@gmail.com>
2023-10-23 11:43:32 +0200

    first commit

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - Entry point
+ * Description: met a la jour la vateur de n à 98 avec un pointeur
+ * @n : variable à modifier
+ * Return:  n
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+
+#endif
