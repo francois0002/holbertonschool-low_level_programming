0c0846f59c6008a916ee4c7fa89ca464b5ef2ece
francois0002 <francois.gosselin.lille@gmail.com>
2023-10-24 09:32:02 +0200

    commit sur fonction reverse string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -3,24 +3,33 @@
 #include "main.h"
 /**
  * rev_string - Entry point
- * Description: afficher une chaine de caractère
- * @str : chaine de caractère
- * Return:  n
+ * Description: afficher une chaine de caractère a l'envers
+ * @s : chaine de caractère
  */
- 
- int _strlen(char *s);
- 
 void rev_string(char *s)
 {
 int longueur = _strlen(s);
-int compteur;
-for (compteur = longueur ; compteur <= 0 ; compteur--)
+int compteur_fin_caractere;
+int compteur_debut_caractere;
+char tmp;
+compteur_fin_caractere = longueur - 1;
+for (compteur_debut_caractere = 0 ; compteur_debut_caractere
+< compteur_fin_caractere ; compteur_debut_caractere++)
 {
-putchar(*s);
+tmp = s[compteur_debut_caractere];
+s[compteur_debut_caractere] = s[compteur_fin_caractere];
+s[compteur_fin_caractere] = tmp;
+
+compteur_fin_caractere--;
 }
 putchar('\n');
 }
-
+/**
+ * _strlen - Entry point
+ * Description: renvoi la longeur d'une chaine de caractère
+ * @s : caractère de  la variable
+ * Return:  n
+ */
 int _strlen(char *s)
 {
 int longueur;
