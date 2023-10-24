03049c6b055697beca44e83e80337cd3a9ca231a
francois0002 <francois.gosselin.lille@gmail.com>
2023-10-24 15:59:20 +0200

    test avec src au lieu de compteur dans le for + betty

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -6,17 +6,17 @@
  * Description: copie la chaine de caractère
  * @dest : copie de src
  * @src : chaine de caractère qui sera copie
+ * Return: dest
  */
- 
+char *_strcpy(char *dest, char *src)
+{
+int compteur;
 
-char *_strcpy(char *dest,  char *src)
+for (compteur = 0 ; src[compteur]  != '\0' ; compteur++)
 {
-int n;
-for (n = 0 ; n != '\0' ; n++)
-{
-dest[n] = src[n];
+dest[compteur] = src[compteur];
 }
-dest[n++] = '\0';
-return(dest);
+dest[compteur++] = '\0';
+return (dest);
 }
 
