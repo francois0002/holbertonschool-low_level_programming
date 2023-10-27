052186480e17473e0f5b3d2f11f35e901a263960
francois0002 <francois.gosselin.lille@gmail.com>
2023-10-27 12:01:16 +0200

    betty

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -2,15 +2,17 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * reset_to_98 - Entry point
+ * _memset - Entry point
  * Description: focntion
  * @b : bite initial
  * @n : nombre de bite à remplir
- * Return: adresse de s 
+ * @s : tableau
+ * Return: adresse de s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0 ; i < n ; i++)
 	{
 	s[i] = b;
