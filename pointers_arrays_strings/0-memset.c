555b035f9b93d9c9f62452fafd17a0520db28aa5
francois0002 <francois.gosselin.lille@gmail.com>
2023-10-27 11:57:26 +0200

    test gcc

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - Entry point
+ * Description: memset 
+ * @b : bite initial
+ * @n : nombre de bite à remplir
+ * Return: adresse de s 
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	for (i = 0 ; i < n ; i++)
+	{
+	s[i] = b;
+	}
+	return (s);
+}
--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,15 +10,28 @@
 
 char *cap_string(char *str)
 {
-	int index = 0;
+	int index = 1;
 	
 
-for (index = 0 ; str[index] >= 'a' && str[index] <= 'z' ; index++)
+for (index = 1 ; str[index] == '\0' ; index++)
 
 {
-		if (str[index - 1] == ' ' ||
-		    str[index - 1] == '\t' ||
-		    str[index - 1] == '\n' ||
+		if (str[index - 1] == ' ')
+		str[index] = str[index] - 32;
+		if (str[index - 1] == ';')
+		str[index] = str[index] - 32;
+		if (str[index - 1] == '.')
+		str[index] = str[index] - 32;
+}
+
+	return (str);
+}
+
+
+/**
+
+
+||
 		    str[index - 1] == ',' ||
 		    str[index - 1] == ';' ||
 		    str[index - 1] == '.' ||
@@ -28,17 +41,20 @@ for (index = 0 ; str[index] >= 'a' && str[index] <= 'z' ; index++)
 		    str[index - 1] == '(' ||
 		    str[index - 1] == ')' ||
 		    str[index - 1] == '{' ||
-		    str[index - 1] == '}' ||
-		    index == 0)
-		str[index] -= str[index] - 32;
-
-	}
-
-	return (str);
-}
+		    str[index - 1] == '}'
+		   )
 
 
-/**|| str[n] = ',' || str[n] = ';' || str[n] = '.'|| str[n] = '!'|| str[n] = '?'|| str[n] = '"'|| str[n] = '('|| str[n] = ')''|| str[n] = '{'|| str[n] = '} 
+
+
+
+
+
+
+
+
+
+|| str[n] = ',' || str[n] = ';' || str[n] = '.'|| str[n] = '!'|| str[n] = '?'|| str[n] = '"'|| str[n] = '('|| str[n] = ')''|| str[n] = '{'|| str[n] = '} 
 
 for (n = 0 ; str[n] = 32; n++)
 
@@ -47,6 +63,9 @@ for (n = 0 ; str[n] = 32; n++)
 || str[n] == ',' || str[n] == ';' || str[n] == '.'|| str[n] == '!'|| str[n] == '?'|| str[n] == '"'|| str[n] == '('|| str[n] == ')' || str[n] == '{'|| str[n] == '}'
 
 if (str[n-1] > 0 && str[n-1] < 65 || str[n-1] > 90 && str[n-1] < 97 || str[n-1] > 122 && str[n-1] < 127)
+
+
+
 
 
 '|| str[i] == ',' || str[i] == ';' || str[i] == '.'|| str[i] == '!'|| str[i] == '?'|| str[i] == '"'|| str[i] == '('|| str[i] == ')' || str[i] == '{'|| str[i] == '}'
