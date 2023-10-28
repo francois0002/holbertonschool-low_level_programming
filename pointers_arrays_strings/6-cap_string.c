eadef2934dc5c266a1e03b65ba58f7bf983ad5bf
francois0002 <francois.gosselin.lille@gmail.com>
2023-10-28 21:32:45 +0200

    betty

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,62 +11,16 @@
 char *cap_string(char *str)
 {
 	int index = 1;
-	
 
-for (index = 1 ; str[index] == '\0' ; index++)
-
-{
+	for (index = 1 ; str[index] == '\0' ; index++)
+	{
 		if (str[index - 1] == ' ')
 		str[index] = str[index] - 32;
 		if (str[index - 1] == ';')
 		str[index] = str[index] - 32;
 		if (str[index - 1] == '.')
 		str[index] = str[index] - 32;
-}
-
+	}
 	return (str);
 }
 
-
-/**
-
-
-||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
-		    str[index - 1] == '.' ||
-		    str[index - 1] == '!' ||
-		    str[index - 1] == '?' ||
-		    str[index - 1] == '"' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}'
-		   )
-
-
-
-
-
-
-
-
-
-
-
-|| str[n] = ',' || str[n] = ';' || str[n] = '.'|| str[n] = '!'|| str[n] = '?'|| str[n] = '"'|| str[n] = '('|| str[n] = ')''|| str[n] = '{'|| str[n] = '} 
-
-for (n = 0 ; str[n] = 32; n++)
-
-
-
-|| str[n] == ',' || str[n] == ';' || str[n] == '.'|| str[n] == '!'|| str[n] == '?'|| str[n] == '"'|| str[n] == '('|| str[n] == ')' || str[n] == '{'|| str[n] == '}'
-
-if (str[n-1] > 0 && str[n-1] < 65 || str[n-1] > 90 && str[n-1] < 97 || str[n-1] > 122 && str[n-1] < 127)
-
-
-
-
-
-'|| str[i] == ',' || str[i] == ';' || str[i] == '.'|| str[i] == '!'|| str[i] == '?'|| str[i] == '"'|| str[i] == '('|| str[i] == ')' || str[i] == '{'|| str[i] == '}'
-*/
