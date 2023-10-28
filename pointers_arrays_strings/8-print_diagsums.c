739114790a1d93e8b2386dfe288e6630855910cb
francois0002 <francois.gosselin.lille@gmail.com>
2023-10-28 12:24:54 +0200

    points 8

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagsums- Entry point
+ * Description: somme de 2 diagonale d'un carré
+ * @a: contenu du tableau
+ * @size: taille
+ * Return: void
+  */
+
+void print_diagsums(int *a, int size)
+{
+*a = 4;
+putchar(a[size] + '\0');
+}
