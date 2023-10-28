980d8188661fddf8f54c8eb68020065fb6e74091
francois0002 <francois.gosselin.lille@gmail.com>
2023-10-28 21:30:25 +0200

    points

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -22,5 +22,5 @@ char *_strncpy(char *dest, char *src, int n)
 			break;
 		}
 	}
-	return (dest);
+
 }
---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * leet- Entry point
+ * Description: affiche un échequier
+ * @s: contenu de l'echiquer
+ * Return: compteur de caractère
+ */
+
+char *leet(char *s)
+{
+return (s);
+}
