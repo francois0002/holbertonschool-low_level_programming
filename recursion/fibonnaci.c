fd0820d04d1d32d5b71789e8fe91ca8a6701e69c
francois0002 <francois.gosselin.lille@gmail.com>
2023-11-08 09:26:12 +0100

    commit vscode fichier un peu partout

---
+++ recursion/fibonnaci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+ 
+ int recursion_fibonnaci(int x)
+ {
+	 int sum = 0;
+	 
+	 if ()
+	 {
+		sum = sum + x;
+		sum = recursion_fibonnaci(x+sum);
+	 }
+ 	return(sum);
+ }
+ 
+ 
+ 
+int main(void)
+{
+recursion_fibonnaci(5);
+
+    return (0);
+}
