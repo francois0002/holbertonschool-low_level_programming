b5dedd2cdf0e40cb604b75c7dff99b3468dd4d7f
francois0002 <francois.gosselin.lille@gmail.com>
2023-11-08 09:22:23 +0100

    betty sur le 3 lignes 40 fonction

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -11,23 +11,19 @@
 
 int **alloc_grid(int width, int height)
 {
-
 	int compteur;
 	int compteur2;
 	int **tab;
-
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-
 	tab = malloc(height * sizeof(int *));
 	if (tab == NULL)
 	{
 		return (NULL);
 	}
-
 	for (compteur = 0 ; compteur < height ; compteur++)
 	{
 		tab[compteur] = malloc(width * sizeof(int));
@@ -41,9 +37,6 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 		}
 	}
-
-
-
 	for (compteur = 0 ; compteur < height ; compteur++)
 	{
 		for (compteur2 = 0 ; compteur2 < width ; compteur2++)
@@ -51,10 +44,5 @@ int **alloc_grid(int width, int height)
 			tab[compteur][compteur2] = 0;
 		}
 	}
-
-
 	return (tab);
 }
-
-
-
