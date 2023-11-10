d953275fe1b531f9d9f144cf1d2d29bbaca2e268
francois0002 <francois.gosselin.lille@gmail.com>
2023-11-10 14:35:58 +0100

    commit avec conditions

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -16,28 +16,47 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 char *fusion;
 int compteur;
 unsigned int compteur2;
+unsigned int comparatif_de_n = _strlen(s2);
+	/**
+	 * si s1 ou s2 est null on les passe en chaine vide
+	  */
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+		if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	/**
+	 * si n est supérieur à la longueur de la string s2 alors n = à s2
+	 */
+	if (n > comparatif_de_n)
+	{
+		n = comparatif_de_n;
+	}
 
-fusion = malloc(sizeof(char) * _strlen(s1) + n + 1);
-if (fusion == NULL)
-{
-	return (NULL);
-}
+	fusion = malloc(sizeof(char) * _strlen(s1) + n + 1);
+	if (fusion == NULL)
+	{
+		return (NULL);
+	}
 
-for (compteur = 0 ; compteur  < _strlen(s1) ; compteur++)
-{
-	fusion[compteur] = s1[compteur];
-}
-for (compteur2 = 0 ; compteur2 < n && s2[compteur2] != '\0'; compteur2++)
-{
-	fusion[_strlen(s1) + compteur2] = s2[compteur2];
-}
+	for (compteur = 0 ; compteur  < _strlen(s1) ; compteur++)
+	{
+		fusion[compteur] = s1[compteur];
+	}
+	for (compteur2 = 0 ; compteur2 < n && s2[compteur2] != '\0'; compteur2++)
+	{
+		fusion[_strlen(s1) + compteur2] = s2[compteur2];
+	}
 
-fusion[_strlen(s1) + n] = '\0';
+	fusion[_strlen(s1) + n] = '\0';
 
 
 
-return (fusion);
-}
+	return (fusion);
+	}
 
 /**
  * _strlen - Entry point
