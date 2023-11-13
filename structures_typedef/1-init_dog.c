b4b6ca37f25871419b2d28180a0469dc666a11d5
francois0002 <francois.gosselin.lille@gmail.com>
2023-11-13 11:02:57 +0100

    change avec malloc sur struct

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -4,23 +4,49 @@
 #include "dog.h"
 
 /**
- * init_dog -initilise une variable sur la structure dog
- * @d: pointeur sur la structure
- * @name: pointeur sur le nom
- * @age: age du chien
- * @owner: pointeur sur le proprio
- */
+* init_dog -initilise une variable sur la structure dog
+* @d: pointeur sur la structure
+* @name: pointeur sur le nom
+* @age: age du chien
+* @owner: pointeur sur le proprio
+*/
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
-
-	d = malloc(sizeof(struct dog));
 	if (d == NULL)
 	{
 		exit(1);
 	}
-	d->name = name;
+	d->name = malloc(_strlen(name) + 1);
+	if (d->name == NULL)
+	{
+		exit(1);
+	}
+	 strcpy(d->name, name);
+
 	d->age = age;
-	d->owner = owner;
+
+	d->owner = malloc(_strlen(owner) + 1);
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		exit(1);
+	}
+	strcpy(d->owner, owner);
+}
+
+/**
+ * _strlen - Entry point
+ * Description: renvoi la longeur d'une chaine de caractère
+ * @s : caractère de  la variable
+ * Return:  longueur
+ */
+int _strlen(char *s)
+{
+int longueur;
+for (longueur = 0 ; *s != '\0' ; longueur++)
+{
+	s++;
+}
+return (longueur);
 }
