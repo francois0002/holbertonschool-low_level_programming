d7df0907abd5d0a4a7125a7173d5e943b9ee619a
francois0002 <francois.gosselin.lille@gmail.com>
2023-11-30 14:32:17 +0100

    first comit delete a node double list

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+/**
+* delete_dnodeint_at_index- delete a node at a given position
+* @head: first punter node
+* @index: position of the node, start to 0
+* Return: 1 if delete sucess or -1 if fail
+*/
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *ptr_node;
+	unsigned int count;
+
+	ptr_node = *head;
+
+	if (*head == NULL)
+	return (-1);
+
+	if (index == 0)
+	{
+		if (ptr_node->next == NULL)
+		{
+			*head = NULL;
+			return (1);
+		}
+		else
+		{
+		ptr_node = ptr_node->next;
+		ptr_node->prev = NULL;
+		*head = ptr_node;
+		return (1);
+		}
+	}
+	for (count = 0 ; count < index ; count++)
+	{
+		if (ptr_node == NULL)
+		{
+			return (-1);
+		}
+		else
+		{
+			ptr_node = ptr_node->next;
+		}
+	}
+	ptr_node->prev->next = ptr_node->next;
+	ptr_node->next->prev = ptr_node->prev;
+	ptr_node = *head;
+	return (1);
+}
