d2c0eead7440f17d4085b89d3e02edcbab8a9f99
francois0002 <francois.gosselin.lille@gmail.com>
2023-12-01 11:12:27 +0100

    commit flip bits

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* flip_bits-  sets the value of a bit to 0 at a given index.
+* @n :numbers
+* @m :numbers
+* Return:the value of the bit at index
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int result;
+
+	n = 0;
+	m = 0;
+
+
+	result = n +  m;
+	return (result);
+}
