2122e667e2ea91ef1a81de5f3492f9a9686bd0cf
francois0002 <francois.gosselin.lille@gmail.com>
2023-12-01 11:09:20 +0100

    first commit clear 4

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* clear_bit-  sets the value of a bit to 0 at a given index.
+* @n :numbers
+* @index :index
+* Return:the value of the bit at index
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+	index = -1;
+
+	*n = index;
+	return (index);
+}
