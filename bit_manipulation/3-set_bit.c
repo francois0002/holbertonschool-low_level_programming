e53d445926588ed09865f247677aa02217593be9
francois0002 <francois.gosselin.lille@gmail.com>
2023-12-01 11:03:23 +0100

    commmit 3 return -1

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* set_bit-  sets the value of a bit to 1 at a given index.
+* @n :numbers
+* @index :index
+* Return:the value of the bit at index
+*/
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int result;
+
+	n = 0;
+	index = -1;
+
+
+	result = n + index;
+	return (result);
+}
