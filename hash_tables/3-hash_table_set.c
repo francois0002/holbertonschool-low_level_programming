7024f2bc7635f9b9a0a684c13ccc0c5c3763277d
francois0002 <francois.gosselin.lille@gmail.com>
2023-12-07 14:14:28 +0100

    commit ht NULL

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = key_index((unsigned char *)key, ht->size);
 	hash_node_t *new_node;
 
-	if (key == NULL || value == NULL)
+	if (key == NULL || value == NULL || ht == NULL)
 	return (0);
 
 
